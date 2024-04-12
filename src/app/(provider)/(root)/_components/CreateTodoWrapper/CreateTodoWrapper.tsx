56891c51ed8edc266d0698e59119ced7cbe7a5c9
yeolsss <yeol8810@gmail.com>
2024-04-12 22:00:55 +0900

    불필요한 component 삭제

--- src/app/(provider)/(root)/_components/CreateTodoWrapper/CreateTodoWrapper.tsx
+++ src/app/(provider)/(root)/_components/CreateTodoWrapper/CreateTodoWrapper.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
-import CreateTodo from "@/app/(provider)/(root)/_components/CreateTodo";
 import { useTodo } from "@/contexts/TodoContext";
 import useInput from "@/hooks/Input";
+import Form from "@/app/(provider)/(root)/_components/CreateTodo/Form";
 
 function CreateTodoWrapper() {
   const [
@@ -17,29 +17,27 @@ function CreateTodoWrapper() {
   const { handleOnSubmitAddTodo } = useTodo();
 
   return (
-    <CreateTodo>
-      <CreateTodo.todoForm
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
-          handleOnSubmitAddTodo(e, todo, setTodo, setTodoValueError, todoRef)
-        }
-      >
-        <CreateTodo.todoForm.input
-          type={"text"}
-          placeholder={"할 일을 입력하세요."}
-          value={todo}
-          onChange={handleOnChangeTodo}
-          className={`border-2 focus:border-green-400 text-black p-2 rounded ${todoValueError && "!border-red-500"}`}
-          ref={todoRef}
-        />
+    <Form
+      onSubmit={(e: React.FormEvent<HTMLFormElement>) =>
+        handleOnSubmitAddTodo(e, todo, setTodo, setTodoValueError, todoRef)
+      }
+    >
+      <Form.input
+        type={"text"}
+        placeholder={"할 일을 입력하세요."}
+        value={todo}
+        onChange={handleOnChangeTodo}
+        className={`border-2 focus:border-green-400 text-black p-2 rounded ${todoValueError && "!border-red-500"}`}
+        ref={todoRef}
+      />
 
-        <CreateTodo.todoForm.button
-          type={"submit"}
-          className="bg-white text-black px-5 rounded hover:bg-gray-800 hover:text-white transition-colors duration-100 ease-in"
-        >
-          등록
-        </CreateTodo.todoForm.button>
-      </CreateTodo.todoForm>
-    </CreateTodo>
+      <Form.button
+        type={"submit"}
+        className="bg-white text-black px-5 rounded hover:bg-gray-800 hover:text-white transition-colors duration-100 ease-in"
+      >
+        등록
+      </Form.button>
+    </Form>
   );
 }
 
